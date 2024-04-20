a7b0906ed97300ab9c9a7d41aba5b890659fc491
Richard Jones <rjones30@gmail.com>
2024-04-20 17:29:12 -0400

    * maybe the packages list must be empty??? [rtj]

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setuptools.setup(
     long_description = long_description,
     long_description_content_type = "text/markdown",
     #packages = setuptools.find_packages(),
-    packages = templates.keys(),
+    packages = [],
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
