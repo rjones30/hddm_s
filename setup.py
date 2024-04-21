83987903cd32b21b425e30adff025e16404b2ed1
Richard Jones <rjones30@gmail.com>
2024-04-20 20:59:58 -0400

    * incremental progress, another bug squashed [rtj]

--- setup.py
+++ setup.py
@@ -65,8 +65,8 @@ class build_ext_with_cmake(build_ext):
             os.environ['HDDM_DIR'] = cwd
             os.environ['LD_LIBRARY_PATH'] += f":{cwd}/build/lib:{cwd}/build/lib64"
             for mod in templates:
-                self.spawn(["bin/hddm-cpp", templates[mod]])
-                self.spawn(["bin/hddm-py", templates[mod]])
+                self.spawn(["build/bin/hddm-cpp", templates[mod]])
+                self.spawn(["build/bin/hddm-py", templates[mod]])
                 self.spawn(["python3", f"setup_{mod}.py"])
 
 
