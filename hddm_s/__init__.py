dec08fac327c75823797e66ad13c4e6bb61e04b1
Richard Jones <rjones30@gmail.com>
2024-04-24 08:12:20 -0400

    * remove the dummy hddm_s directory, it is not needed [rtj]

--- hddm_s/__init__.py
+++
@@ -1,7 +0,0 @@
-#
-# hddm_s module - python wrapper around c++ i/o library for 
-#                 simulated events from the GlueX experiment.
-#
-# author: richard.t.jones at uconn.edu
-# version: april 24, 2024
-#
