fce8ef8eb9a1bbc4833471b770481ef035d26823
Richard Jones <rjones30@gmail.com>
2024-04-25 14:50:15 -0400

    * try another alternative, the -l: syntax for link lists [rtj]

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "1.0.30",
+    version = "1.0.31",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
@@ -113,7 +113,7 @@ setuptools.setup(
            include_dirs = [".", "../build/include"],
            library_dirs = ["../build/lib", "../build/lib64"],
            libraries = ["xstream", "bz2", "z", 
-                        "hdf5.a", "hdf5_hl.a",
+                        ":libhdf5.a", ":libhdf5_hl.a",
                        ],
            extra_compile_args = ["-std=c++11", "-DHDF5_SUPPORT"],
            sources = ["hddm_s++.cpp", "pyhddm_s.cpp"]),
