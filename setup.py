374323c41b9bf7814dd8d27ba7587880c754a964
Richard Jones <rjones30@gmail.com>
2024-04-30 11:12:28 -0400

    * bump the version number [rtj]

--- setup.py
+++ setup.py
@@ -125,7 +125,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "1.0.42",
+    version = "1.0.43",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
