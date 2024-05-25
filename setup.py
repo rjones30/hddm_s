418c1a365e868862c7c9fc09c307f899407f6de1
Richard Jones <rjones30@gmail.com>
2024-05-24 20:22:26 -0400

    * setting BUILD_SHARED_LIBS=OFF breaks the windows build, only   select that setting on non-windows platforms [rtj]

--- setup.py
+++ setup.py
@@ -84,6 +84,8 @@ class build_ext_with_cmake(build_ext):
         ]
         if sysconfig.get_platform() == "win32":
             cmake_args += ["-A", "Win32"]
+        if "win" in sysconfig.get_platform() and ext.name == "xerces-c":
+            cmake_args += [f"-DBUILD_SHARED_LIBS:BOOL=ON"]
         self.spawn([cmake, f"../{ext.name}"] + cmake_args)
         if not self.dry_run:
             self.spawn([cmake, "--build", "."] + build_args + ["-j4"])
@@ -167,7 +169,7 @@ else:
                           ]
 setuptools.setup(
     name = "hddm_s",
-    version = "1.0.73",
+    version = "1.0.74",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
