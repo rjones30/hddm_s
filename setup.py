a591be5e18bb2bffc3a44e3ea4f33278fd867ddc
Richard Jones <rjones30@gmail.com>
2024-06-03 17:51:33 -0400

    * link with libXrdCl_static instead of shared [rtj]

--- setup.py
+++ setup.py
@@ -188,11 +188,11 @@ else:
                            "ssl3",
                            "crypto",
                            "xrootdstream",
-                           "XrdCl",
+                           "XrdCl_static",
                           ]
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.8",
+    version = "2.0.9",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
