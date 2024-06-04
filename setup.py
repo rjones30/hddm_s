4cfe576e3fc1686f47c7f4bf2ee5906c549e83e9
Richard Jones <rjones30@gmail.com>
2024-06-04 10:19:23 -0400

    * new init submodules step in HDDM, bump the version [rtj]

--- setup.py
+++ setup.py
@@ -196,7 +196,7 @@ else:
                           ]
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.15",
+    version = "2.0.16",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
