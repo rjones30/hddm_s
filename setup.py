0791e1f5aded3aa2cf8cde6518f6111198b3b618
Richard Jones <rjones30@gmail.com>
2024-06-11 11:34:40 -0400

    * more work on xrootd ubuntu builder, bump the version [rtj]

--- setup.py
+++ setup.py
@@ -217,7 +217,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.41",
+    version = "2.0.42",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
