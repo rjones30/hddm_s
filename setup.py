917e47482578521a09466b37b7ce98d3a49077ce
Richard Jones <rjones30@gmail.com>
2024-06-11 17:03:31 -0400

    * at a loss for why xrootd is so resistant to building on ubuntu [rtj]

--- setup.py
+++ setup.py
@@ -217,7 +217,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.47",
+    version = "2.0.48",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
