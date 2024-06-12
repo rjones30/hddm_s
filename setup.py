2b806f2c7007ebd9474747c57f02e1918ed7669d
Richard Jones <rjones30@gmail.com>
2024-06-12 13:37:54 -0400

    * super difficult to build xrootd with cmake [rtj]

--- setup.py
+++ setup.py
@@ -217,7 +217,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.57",
+    version = "2.0.58",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
