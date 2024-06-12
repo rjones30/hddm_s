9d8ce494be6f645f1c2171905e4580f57240c5b2
Richard Jones <rjones30@gmail.com>
2024-06-12 08:23:09 -0400

    * a hard lesson learned in cmake string comparisons, bump the version [rtj]

--- setup.py
+++ setup.py
@@ -217,7 +217,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.51",
+    version = "2.0.52",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
