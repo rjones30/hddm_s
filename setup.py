2c79b5b24fcded358bbad3403584651a0321162c
Richard Jones <rjones30@gmail.com>
2024-06-12 12:59:44 -0400

    * still working on xrootd cmake builder [rtj]

--- setup.py
+++ setup.py
@@ -217,7 +217,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.56",
+    version = "2.0.57",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
