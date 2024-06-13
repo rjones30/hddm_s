b2fe738aafbcf67d5dc1393fa20145929ae1aad6
Richard Jones <rjones30@gmail.com>
2024-06-12 22:50:59 -0400

    * what goes bump in the night? [rtj]

--- setup.py
+++ setup.py
@@ -217,7 +217,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.66",
+    version = "2.0.67",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
