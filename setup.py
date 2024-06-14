3cfc6495f002dcb4d9197108dacab0d0e928fbcd
Richard Jones <rjones30@gmail.com>
2024-06-14 17:42:14 -0400

    * another round with cmake xrootd on macos13 -rtj-

--- setup.py
+++ setup.py
@@ -218,7 +218,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.92",
+    version = "2.0.93",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
