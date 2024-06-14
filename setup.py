975c8702e4984b4c9f7b499680879604f288f5f9
Richard Jones <rjones30@gmail.com>
2024-06-14 19:22:35 -0400

    * this is like debugging in the punch-card era: submit, wait, try again [rtj]

--- setup.py
+++ setup.py
@@ -218,7 +218,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.94",
+    version = "2.0.95",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
