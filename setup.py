a0e638c1b7c7157103f2f194b4dd382b8c0cc0df
Richard Jones <rjones30@gmail.com>
2024-06-14 15:01:02 -0400

    * fix a typo in the xrootd cmake files [rtj]

--- setup.py
+++ setup.py
@@ -218,7 +218,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.87",
+    version = "2.0.88",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
