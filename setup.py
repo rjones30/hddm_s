54d35424a68c919be00229eddf6405b8db2c95f3
Richard Jones <rjones30@gmail.com>
2024-06-14 14:01:23 -0400

    bruised and battered, but carrying on [rtj]

--- setup.py
+++ setup.py
@@ -218,7 +218,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.86",
+    version = "2.0.87",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
