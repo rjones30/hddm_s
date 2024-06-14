008e0f8ae9cbed51703850040254ce4ea68faab2
Richard Jones <rjones30@gmail.com>
2024-06-14 18:55:31 -0400

    * let's diagnose, shall we? [rtj]

--- setup.py
+++ setup.py
@@ -218,7 +218,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.93",
+    version = "2.0.94",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
