f2c813cd3d3ca318d4b3101aed5b6f7084c179f9
Richard Jones <rjones30@gmail.com>
2024-06-14 11:46:10 -0400

    * diagnostics fix in xrootd build [rtj]

--- setup.py
+++ setup.py
@@ -218,7 +218,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.84",
+    version = "2.0.85",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
