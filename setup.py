da894e273d7e8bde67453f42c4a6ef696c83507c
Richard Jones <rjones30@gmail.com>
2024-06-14 09:15:03 -0400

    * still busting *** on linking xrootd on macos13 [rtj]

--- setup.py
+++ setup.py
@@ -218,7 +218,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.80",
+    version = "2.0.81",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
