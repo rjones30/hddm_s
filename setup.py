10fd2e5284ef3f3949bce8722d8d16fa2f002ad6
Richard Jones <rjones30@gmail.com>
2024-06-14 15:30:59 -0400

    * a race on ubuntu libuuid cmaker, bump and try again [rtj]

--- setup.py
+++ setup.py
@@ -218,7 +218,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.88",
+    version = "2.0.89",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
