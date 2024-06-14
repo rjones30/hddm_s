0d2d6d36988c0fc79ad362ec6bc7356637edc5ba
Richard Jones <rjones30@gmail.com>
2024-06-14 09:21:06 -0400

    * when ubuntu ci action fails in first few minutes, do again [rtj]

--- setup.py
+++ setup.py
@@ -218,7 +218,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.81",
+    version = "2.0.82",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
