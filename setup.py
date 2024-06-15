84b64639b8b6dfb4339d5074989d67514effb742
Richard Jones <rjones30@gmail.com>
2024-06-15 13:58:42 -0400

    * another bump, race condition in ubuntu build, ignoring for now [rtj]

--- setup.py
+++ setup.py
@@ -221,7 +221,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.110",
+    version = "2.0.111",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
