1bc604cd4a8b03cc80148c5c29c670baede1b253
Richard Jones <rjones30@gmail.com>
2024-06-15 11:12:47 -0400

    * bump again, no way that error is reproducible ... -rtj-

--- setup.py
+++ setup.py
@@ -220,7 +220,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.108",
+    version = "2.0.109",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
