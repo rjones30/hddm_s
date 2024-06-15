720966b4e6f46eb7c5d07cec101a49f11c7ec1b7
Richard Jones <rjones30@gmail.com>
2024-06-14 21:05:18 -0400

    * expand the search for system headers [rtj]

--- setup.py
+++ setup.py
@@ -218,7 +218,7 @@ else:
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.98",
+    version = "2.0.99",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
