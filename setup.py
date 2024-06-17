56a15fe083bb0397f94a00a1991c6fb39b911f21
Richard Jones <rjones30@gmail.com>
2024-06-17 15:03:11 -0400

    * another fix to the HDDM cmake files [rtj]

--- setup.py
+++ setup.py
@@ -235,7 +235,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.141",
+    version = "2.0.142",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
