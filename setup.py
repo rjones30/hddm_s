5a76b40eef9b54591abdf6b46e5649a434370b6f
Richard Jones <rjones30@gmail.com>
2024-06-17 05:21:50 -0400

    * make unique names for each platform, python flavour [rtj]

--- setup.py
+++ setup.py
@@ -233,7 +233,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.129",
+    version = "2.0.130",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
