f10662bcd1d33c312d557f5025cae9d75301bb7d
Richard Jones <rjones30@gmail.com>
2024-06-18 04:34:28 -0400

    * more fixes to the cpr install scripts for windows [rtj]

--- setup.py
+++ setup.py
@@ -240,7 +240,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.153",
+    version = "2.0.154",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
