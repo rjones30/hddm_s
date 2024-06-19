bedd849bcaea72a73c434bd89973a3a0b730c202
Richard Jones <rjones30@gmail.com>
2024-06-19 13:34:05 -0400

    * a bit of thrashing around hoping to stir something up [rtj]

--- setup.py
+++ setup.py
@@ -240,7 +240,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.179",
+    version = "2.0.180",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
