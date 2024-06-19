caf5f265bd4aa9305546f83c3bf5cc31450f0716
Richard Jones <rjones30@gmail.com>
2024-06-19 07:17:08 -0400

    * openssl installer script fixes for windows [rtj]

--- setup.py
+++ setup.py
@@ -240,7 +240,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "hddm_s",
-    version = "2.0.168",
+    version = "2.0.169",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
