486fefdc6f13f95c04688478c8739d0d84a800f2
Richard Jones <rjones30@gmail.com>
2024-06-25 15:35:26 -0400

    * regenerate wheels without the dummy __init__.py placeholders [rtj]

--- setup.py
+++ setup.py
@@ -234,7 +234,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "gluex.hddm_s",
-    version = "1.0.7",
+    version = "1.0.8",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
