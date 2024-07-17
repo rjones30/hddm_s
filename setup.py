ca461a4eba38e74ba3ba08c5be395c2a7529c341
Richard Jones <rjones30@gmail.com>
2024-07-16 23:48:03 -0400

    * trying to set sys.path to find pip [rtj]

--- setup.py
+++ setup.py
@@ -84,13 +84,13 @@ class build_ext_with_cmake(build_ext):
             cmake = ["cmake.exe"]
         if "zlib" in ext.name:
             # Needed by xrootd installation scripts
+            sys.path.append("/opt/python/cp36-cp36m/lib/python3.6/site-packages")
             print(f"sys.executable is {sys.executable}")
             print(f"sys.path is {sys.path}")
             print(f"os.environ['PYTHONPATH'] is {os.environ['PYTHONPATH']}")
-            print(f"os.environ['HOME'] is {os.environ['HOME']}")
-            self.spawn(["curl", "https://bootstrap.pypa.io/pip/3.6/get-pip.py",
-                                "-o", "get-pip.py"])
-            exec(open("get-pip.py").read())
+            #self.spawn(["curl", "https://bootstrap.pypa.io/pip/3.6/get-pip.py",
+            #                    "-o", "get-pip.py"])
+            #exec(open("get-pip.py").read())
             import pip
             self.spawn(["python", "-m", "pip", "-V"])
 
@@ -246,7 +246,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "gluex.hddm_s",
-    version = "2.0.10",
+    version = "2.0.11",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
