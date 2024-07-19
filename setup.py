384e489b5b9ffd2bfe5867b4d16b4f2877e6414a
Richard Jones <rjones30@gmail.com>
2024-07-18 22:11:35 -0400

    * final fixes to xrootd, HDDM for xrootd version 5.6 [rtj]

--- setup.py
+++ setup.py
@@ -245,7 +245,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "gluex.hddm_s",
-    version = "2.0.25",
+    version = "2.0.26",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
