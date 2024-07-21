136e70b95b7d005e45078a74f7b1b37b5a9483d8
Richard Jones <rjones30@gmail.com>
2024-07-20 23:03:21 -0400

    * try an innovative way to set package_data to a dynamic list [rtj]

--- setup.py
+++ setup.py
@@ -179,23 +179,6 @@ class install_ext_solibs(install_lib):
                 for mext in re.finditer("^([^/]*).cpython.*", solib):
                     if not mext.group(1) in templates:
                         self.spawn(["rm", "-f", f"{wheel}/{solib}"])
-            print(f"install_ext_solibs in gluex tree:")
-            for mext in glob.glob("build/lib*/python*/site-packages"):
-               self.spawn(["ls", "-lR", mext])
-               print(f"copying site-packages into gluex...")
-               tarball = "build/site_packages.tar"
-               self.spawn(["tar", "-cf", tarball, "-C", mext, "."])
-               self.spawn(["tar", "-xf", tarball, "-C", f"gluex"])
-               for solibdir in glob.glob("build/lib*"):
-                  cwd = os.getcwd()
-                  os.chdir(solibdir)
-                  solibs = glob.glob("*.so*")
-                  solibs += glob.glob("*.dylib*")
-                  os.chdir(cwd)
-                  if len(solibs) > 0:
-                     self.spawn(["tar", "-cf", tarball, "-C", solibdir] + solibs)
-                     self.spawn(["tar", "-xf", tarball, "-C", f"gluex/pyxrootd"])
-               self.spawn(["ls", "-lR", "gluex"])
  
 
 with open("README.md", "r") as fh:
@@ -271,19 +254,12 @@ setuptools.setup(
     packages = templates.keys(),
     namespace_packages=['gluex'],
     package_data = templates,
-    data_files = [("gluex/pyxrootd", glob.glob("gluex/pyxrootd/*")),
-                  ("gluex/XRootD/client", glob.glob("gluex/XRootD/client/*")),
-                  ("gluex", glob.glob("xrootd*")),
-                 ],
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],                                      # Information to filter the project on PyPi website
     python_requires = '>=3.6',              # Minimum version requirement of the package
-    #install_requires = [                   # Install other dependencies if any
-    #  "setuptools-git",
-    #],
     ext_modules = [
       CMakeExtension("zlib"),
       CMakeExtension("bzip2"),
@@ -295,6 +271,61 @@ setuptools.setup(
       CMakeExtension("cpr"),
       CMakeExtension("xrootd"),
       CMakeExtension("HDDM"),
+    ],
+    cmdclass = {
+      "build_ext": build_ext_with_cmake,
+      "install_lib": install_ext_solibs,
+    }
+)
+
+# I need to run setup a second time here because only after the
+# preceeding builds can I know the names of all of the shared
+# libraries and dynamically generated python classes that I need
+# to include as package data in my module.
+
+for wheel in glob.glob("build/bdist.*/wheel"):
+    print(f"install_ext_solibs in gluex tree:")
+    for mext in glob.glob("build/lib*/python*/site-packages"):
+        self.spawn(["ls", "-lR", mext])
+        print(f"copying site-packages into gluex...")
+        tarball = "build/site_packages.tar"
+        self.spawn(["tar", "-cf", tarball, "-C", mext, "."])
+        self.spawn(["tar", "-xf", tarball, "-C", f"gluex/hddm_s"])
+        for solibdir in glob.glob("build/lib*"):
+            cwd = os.getcwd()
+            os.chdir(solibdir)
+            solibs = glob.glob("*.so*")
+            solibs += glob.glob("*.dylib*")
+            os.chdir(cwd)
+            if len(solibs) > 0:
+                self.spawn(["tar", "-cf", tarball, "-C", solibdir] + solibs)
+                self.spawn(["tar", "-xf", tarball, "-C", f"gluex/hddm_s/pyxrootd"])
+
+package_data = {"gluex.hddm_s": [ "rest.xml",
+                                  "pyxrootd/*",
+                                  "XRootD/*",
+                                  "xrootd*/*",
+                                ]
+               }
+
+setuptools.setup(
+    name = "gluex.hddm_s",
+    version = "2.1.13",
+    url = "https://github.com/rjones30/hddm_s",
+    author = "Richard T. Jones",
+    description = "i/o module for GlueX simulated events",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages = templates.keys(),
+    namespace_packages=['gluex'],
+    package_data = package_data,
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires = '>=3.6',              # Minimum version requirement of the package
+    ext_modules = [
       setuptools.Extension("gluex.hddm_s",
            include_dirs = extension_include_dirs,
            library_dirs = extension_library_dirs,
