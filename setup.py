18170d35734ed79d72e5794b2997608b442b90a4
Richard T. Jones <jonesrt@gluey.phys.uconn.edu>
2024-08-02 10:40:51 -0400

    * fix a typo in the last commit [rtj]

--- setup.py
+++ setup.py
@@ -284,11 +284,11 @@ setuptools.setup(
     long_description_content_type = "text/markdown",
     packages = templates.keys(),
     namespace_packages=['gluex'],
-    package_data = ["gluex.hddm_s": ["event.xml",
+    package_data = {"gluex.hddm_s": ["event.xml",
                                      "site_packages.tar.gz",
                                      "sharedlibs.tar.gz",
                                     ],
-    ],
+    },
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
