a3c858ebcd3a412bb9fbc177b4f5b4f4bf2627e1
Richard T. Jones <jonesrt@gluey.phys.uconn.edu>
2024-10-31 04:15:45 -0400

    * advance minor version number to 2.3 [rtj]

--- setup.py
+++ setup.py
@@ -262,7 +262,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "gluex.hddm_s",
-    version = "2.2.5",
+    version = "2.3.0",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
