a6af9ae2d177aa162905d70ddea06eb5e984f697
Richard Jones <rjones30@gmail.com>
2025-12-25 09:47:29 -0500

    * customize some build options on windows, avoid pp38-* which is known   to behave in strange ways, first good version on pypy is pp38-* [rtj]

--- setup.py
+++ setup.py
@@ -275,7 +275,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "gluex.hddm_s",
-    version = "2.4.00",
+    version = "2.4.0",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
