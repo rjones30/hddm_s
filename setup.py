b43ed2932ec959c6ad01f11eeb129c01d035afe8
Richard Jones <rjones30@gmail.com>
2025-12-28 05:22:57 -0500

    * update to tag 2.4.1 [rtj]

--- setup.py
+++ setup.py
@@ -281,7 +281,7 @@ if "macos" in sysconfig.get_platform():
 
 setuptools.setup(
     name = "gluex.hddm_s",
-    version = "2.4.0",
+    version = "2.4.1",
     url = "https://github.com/rjones30/hddm_s",
     author = "Richard T. Jones",
     description = "i/o module for GlueX simulated events",
